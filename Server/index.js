d4cfcc7d017ef478a5836a3baf189f08df57ee64
CoderShivam2005 <sm6718858@gmail.com>
2025-12-10 17:16:39 +0530

    Deployemnet issue

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:[process.env.FRONTEND_URL],
+  origin:["https://skill-engine.vercel.app/",
+  "http://localhost:5173"],
   methods:['GET','POST','PUT','DELETE','PATCH'],
   credentials:true
 }));
