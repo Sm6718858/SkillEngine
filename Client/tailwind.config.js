f2c185ab44ce9dfe0b5ad7c4c3edb15c7c092b1e
CoderShivam2005 <sm6718858@gmail.com>
2025-12-12 23:18:46 +0530

    testing

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -4,6 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+   safelist: [
+    "translate-x-0",
+    "-translate-x-full",
+    "md:translate-x-0",
+    "transition-transform",
+    "duration-300",
+    "ease-in-out"
+  ],
   theme: {
     extend: {},
   },
